b884b5b62d90476d619e0be4cf0084a6a1ce8738
GeniusPhysics <gabrieljorge343@gmail.com>
2024-09-07 16:16:28 -0300

    test: Criar testes para as classes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "123456789", modelo: "Nokia 3310", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "987654321", modelo: "Iphone 12", imei: "222222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
